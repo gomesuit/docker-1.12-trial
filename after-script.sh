b6647f95a9bec2ffc7845a9ee28d0ac8e3b82aae
gomesuit <takashi.inoue@theport.jp>
2017-01-21 03:06:03 +0900

    delete

--- after-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-vagrant ssh manager -c 'docker swarm init --advertise-addr enp0s8'
-
-JOIN_TOKEN=$(vagrant ssh manager -c 'docker swarm join-token worker -q' | sed '$s/.$//')
-
-vagrant ssh node1 -c "docker swarm join --token $JOIN_TOKEN 192.168.33.10:2377"
-vagrant ssh node2 -c "docker swarm join --token $JOIN_TOKEN 192.168.33.10:2377"
-
